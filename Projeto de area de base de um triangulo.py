52e4ebff27daa9e06595ace13bd73a8064778101
SamuelLucass <Samuka.silva@outlook.com>
2022-11-06 23:07:49 -0300

    Projeto da area de base de um Cone em Python

---
+++ Projeto de area de base de um triangulo.py
@@ -0,0 +1,9 @@
+#Criar um programa que calcula o volume de um cone
+h = float(input("Digite a altura do cone: "))
+r = float(input("Digite o raio do cone: "))
+pi = (float(3.141592))
+
+#efetua a equação de volume de um triangulo
+v = pi * r * r * h / 3
+print(v)
+exit()
